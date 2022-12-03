f3a6900be8cf4a796d12a8a068aaa814f3c9e992
Churchill <ulthanash.shelwe@gmail.com>
2022-12-03 21:22:29 +0000

    Added parser and data. Need further tests.

--- ParserItems.py
+++ ParserItems.py
@@ -1,9 +1,19 @@
 import Item
 
+class WeaponPool:
+    @staticmethod
+    def parse(directory):
+        out = []
+        f = open(directory, "r")
+        for line in f:
+            parsed_line = eval(line)
+            req_fun = Item.Requirements(parsed_line[2])
+            newitem = Item.Weapon(parsed_line[0], parsed_line[1], req_fun)
+            out.append(newitem)
+        return out
 
-class ItemPool:
-    def __init__(self):
-        self.itempool = []
+    def __init__(self, directory):
+        self.pool = self.parse(directory)
 
     def searchitem(self, name):
-        return [item for item in self.itempool if name in item.name]
+        return [item for item in self.pool if name in item.name]
