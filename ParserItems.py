d6a51c3905edf34ea130b9e74b37edbf060b3d1f
Churchill <ulthanash.shelwe@gmail.com>
2023-01-07 17:45:40 +0000

    Tested the parser. Seems to work

--- ParserItems.py
+++ ParserItems.py
@@ -1,6 +1,11 @@
 import Item
 
-class WeaponPool:
+
+class Pool:
+    """
+    Class has only one attribute so far, pool, containing a list of instances of items. It provides creation and search
+    methods on the pool.
+    """
     @staticmethod
     def parse(directory):
         out = []
@@ -8,12 +13,19 @@ class WeaponPool:
         for line in f:
             parsed_line = eval(line)
             req_fun = Item.Requirements(parsed_line[2])
-            newitem = Item.Weapon(parsed_line[0], parsed_line[1], req_fun)
-            out.append(newitem)
+            new_item = Item.Weapon(parsed_line[0], parsed_line[1], req_fun)
+            out.append(new_item)
         return out
 
     def __init__(self, directory):
         self.pool = self.parse(directory)
 
-    def searchitem(self, name):
+    def __str__(self):
+        out = "List of all the elements of this item pool:\n"
+        for item in self.pool:
+            out += item.pretty_string()
+            out += "\n"
+        return out
+
+    def search_item(self, name):
         return [item for item in self.pool if name in item.name]
