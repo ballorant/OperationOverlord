06119faa13ee48f5d2a7c003e207ef48a448415b
Churchill <ulthanash.shelwe@gmail.com>
2023-01-07 17:53:31 +0000

    Added generic class for item parsing and generic item pool generation. Cannot handle mixed item type database.

--- ParserItems.py
+++ ParserItems.py
@@ -2,6 +2,32 @@ import Item
 
 
 class Pool:
+    def __init__(self, directory):
+        self.pool = self.parse(directory)
+
+    def search_item(self, name):
+        return [item for item in self.pool if name in item.name]
+
+    def __str__(self):
+        out = "List of all the elements of this item pool:\n"
+        for item in self.pool:
+            out += item.pretty_string()
+            out += "\n"
+        return out
+
+    @staticmethod
+    def parse(directory):
+        out = []
+        f = open(directory, "r")
+        for line in f:
+            parsed_line = eval(line)
+            req_fun = Item.Requirements(parsed_line[1])
+            new_item = Item.Item(parsed_line[0], req_fun)
+            out.append(new_item)
+        return out
+
+
+class WeaponPool(Pool):
     """
     Class has only one attribute so far, pool, containing a list of instances of items. It provides creation and search
     methods on the pool.
@@ -17,15 +43,9 @@ class Pool:
             out.append(new_item)
         return out
 
-    def __init__(self, directory):
-        self.pool = self.parse(directory)
-
     def __str__(self):
         out = "List of all the elements of this item pool:\n"
         for item in self.pool:
             out += item.pretty_string()
             out += "\n"
         return out
-
-    def search_item(self, name):
-        return [item for item in self.pool if name in item.name]
